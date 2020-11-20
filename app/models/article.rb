39c5382f57a93348e9a74642f8f9a165f575b21c
enaburekhan <ericenaburekhan2018@gmail.com>
2020-11-20 12:14:27 +0100

    create model and schema

---
+++ app/models/article.rb
@@ -0,0 +1,4 @@
+class Article < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 3, maximum: 20 } 
+  validates :description, presence: true, length: { minimum: 10, maximum: 100 }   
+end
