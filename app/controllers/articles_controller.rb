223b678f4883034f4531c59f53503fca77357a48
enaburekhan <ericenaburekhan2018@gmail.com>
2020-11-21 12:51:33 +0100

    create edit, index, update and show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
 
   def new
     @article = Article.new
@@ -12,6 +15,20 @@ class ArticlesController < ApplicationController
     else
       render 'new'    
     end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = 'Article was sucessfully updated'
+      redirect_to article_path(@article)
+    else
+      render 'new'          
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def show
